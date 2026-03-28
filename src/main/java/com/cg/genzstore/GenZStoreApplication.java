0171961d17cfe672fb7c9d4c1dc6c525ce0dccfc
dhanu <dhanushcg100@gmail.com>
2026-03-28 10:12:35 +0530

    initialized the project

---
+++ src/main/java/com/cg/genzstore/GenZStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cg.genzstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenZStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenZStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/genzstore/GenzstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.genzstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenzstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
