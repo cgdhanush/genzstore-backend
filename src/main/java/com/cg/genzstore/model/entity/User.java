3fdbf308ab492d29a7d39ae2c038a7246f84e9da
dhanu <dhanushcg100@gmail.com>
2026-03-30 17:10:56 +0530

    updated user entity

--- src/main/java/com/cg/genzstore/model/entity/User.java
+++ src/main/java/com/cg/genzstore/model/entity/User.java
@@ -1,10 +1,32 @@
 package com.cg.genzstore.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
     private Long id;
+    
     private String name;
+
+    @Column(unique = true)
     private String email;
+    
     private String password;
-    private String role;
+    
+    private String role; // USER / ADMIN
 
 }
