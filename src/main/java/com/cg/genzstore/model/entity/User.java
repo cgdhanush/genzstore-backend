b8180e48baae20599d795ac6a1febc3092cef3a4
dhanu <dhanushcg100@gmail.com>
2026-03-30 16:55:46 +0530

    added user entity

---
+++ src/main/java/com/cg/genzstore/model/entity/User.java
@@ -0,0 +1,10 @@
+package com.cg.genzstore.model.entity;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+}
