49ce82c7e40e21c447ff6f1774feb45957737a27
dhanu <dhanushcg100@gmail.com>
2026-03-30 16:57:08 +0530

    added product entity

---
+++ src/main/java/com/cg/genzstore/model/entity/Product.java
@@ -0,0 +1,9 @@
+package com.cg.genzstore.model.entity;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
+    private String stock;
+}
