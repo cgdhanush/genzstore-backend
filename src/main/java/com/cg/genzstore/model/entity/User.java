7bf172e8b1420595462468846dee57c915937cf4
dhanu <dhanushcg100@gmail.com>
2026-03-30 17:26:02 +0530

    added ddl-auto

--- src/main/java/com/cg/genzstore/model/entity/User.java
+++ src/main/java/com/cg/genzstore/model/entity/User.java
@@ -2,6 +2,8 @@ package com.cg.genzstore.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,15 +20,19 @@ import lombok.Setter;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(length = 50)
     private String name;
 
-    @Column(unique = true)
+    @Column(unique = true, length = 50)
     private String email;
     
+    @Column(length = 20)
     private String password;
     
+    @Column(length = 20)
     private String role; // USER / ADMIN
 
 }
