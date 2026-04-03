7c5e859af7bdee97434f024edf292b51c5094508
dhanu <dhanushcg100@gmail.com>
2026-04-03 22:11:29 +0530

    updated prosuct entity for mongodb

--- src/main/java/com/cg/genzstore/model/entity/Product.java
+++ src/main/java/com/cg/genzstore/model/entity/Product.java
@@ -1,7 +1,22 @@
 package com.cg.genzstore.model.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Document(collection = "products")
 public class Product {
-    private Long id;
+
+    @Id
+    private String id;
     private String name;
     private String description;
     private Integer price;
--- src/main/java/com/cg/genzstore/model/entity/User.java
+++ src/main/java/com/cg/genzstore/model/entity/User.java
@@ -16,13 +16,9 @@ public class User {
 
     @Id
     private String id;
-    
     private String name;
-
     private String email;
-    
     private String password;
-    
-    private String role; // USER / ADMIN
+    private String role; // USER / OWNER / ADMIN
 
 }
