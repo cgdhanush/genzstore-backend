24959a56949a182e0eb21237a5577daaa6d43f99
dhanu <dhanushcg100@gmail.com>
2026-05-10 00:43:24 +0530

    removed unused imports

--- src/main/java/com/cg/genzstore/controllers/UserController.java
+++ src/main/java/com/cg/genzstore/controllers/UserController.java
@@ -1,18 +1,14 @@
 package com.cg.genzstore.controllers;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.genzstore.model.dto.UserDTO;
 import com.cg.genzstore.model.entity.User;
 import com.cg.genzstore.service.JwtService;
 import com.cg.genzstore.service.UserService;
--- src/main/java/com/cg/genzstore/service/ProductService.java
+++ src/main/java/com/cg/genzstore/service/ProductService.java
@@ -3,7 +3,6 @@ package com.cg.genzstore.service;
 import java.io.IOException;
 import java.util.List;
 
-import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
